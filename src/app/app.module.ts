d07c6275b05410ebdbde0ba3de5b710d958455e7
mirek001 <mirek.grzegorczyk@gmail.com>
2019-03-03 11:25:28 +0000

    add 'ngx-md' module and settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxMdModule } from 'ngx-md'; //ngx-md module
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxMdModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
