84bfdda2b293752a4f699b7c65f5e1b2a95db803
mirek001 <mirek.grzegorczyk@gmail.com>
2019-03-04 13:12:07 +0000

    update config file

--- src/app/themes/bootstrap-compact-theme/bootstrap-compact-theme.component.ts
+++ src/app/themes/bootstrap-compact-theme/bootstrap-compact-theme.component.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-bootstrap-compact-theme',
   templateUrl: './bootstrap-compact-theme.component.html',
   styleUrls: ['./bootstrap-compact-theme.component.css']
 })
-export class BootstrapCompactThemeComponent implements OnInit {
+export class BootstrapCompactThemeComponent implements OnChanges, OnInit {
 
   @Input() configJson;
   @Input() menu_items;
 
-project_title = this.configJson.project_title;
+//project_title = this.configJson.project_title;
+project_title = "manual";
+
 
   constructor() { }
 
   ngOnInit() {
+    console.log("W compakcie:", this.configJson);
+  }
+
+  ngOnChanges(){
+    this.project_title = this.configJson.config.project_title;
+    console.log("TYTUL: ", this.configJson.config.project_title)
   }
 
 }
